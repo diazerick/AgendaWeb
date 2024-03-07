9287a1ef759e39e7d4ed882831b36d792ec0c287
erick <erick@ERICK>
2024-03-06 19:21:32 -0600

    Agregar archivos de proyecto.

---
+++ SolAgenda/WebAgenda/Content/dist/js/global.js
@@ -0,0 +1,105 @@
+﻿function creaFecha(fecha, hora = '') {
+    //Crea objeto fecha a partir de fecha en dd/mm/yyyy y hora
+    let anio = fecha.substring(6)
+    let mes = fecha.substring(3, 5)
+    let dia = fecha.substring(0, 2)
+    let sFecha = `${anio}-${mes}-${dia} ${hora}`
+    sFecha = sFecha.trim()
+    if (new Date(sFecha).toString() == 'Invalid Date') return null
+    fecha = `${anio}-${mes}-${dia}`
+    if (hora != '')
+        fecha += ' ' + hora
+    return fecha
+}
+
+function formatFecha(formatoOriginal) {
+    var partes = formatoOriginal.split('-')
+    var fecha = new Date(partes[0], partes[1] - 1, partes[2])
+
+    var dia = fecha.getDate()
+    var mes = fecha.getMonth() + 1
+    var anio = fecha.getFullYear()
+
+    dia = dia < 10 ? '0' + dia : dia
+    mes = mes < 10 ? '0' + mes : mes
+
+    var formatoConvertido = dia + '/' + mes + '/' + anio
+    return formatoConvertido
+}
+
+function muestraMensaje(icon, titulo, mensaje) {
+    Swal.fire({
+        icon: icon,
+        title: titulo,
+        text: mensaje,
+        confirmButtonColor: '#F36F21'
+    })
+}
+
+function muestraMensajeConfirmacion(mensaje, funcion) {
+    Swal.fire({
+        icon: 'question',
+        title: 'Confirmación',
+        text: mensaje,
+        showCancelButton: true,
+        confirmButtonText: 'Confirmar',
+        cancelButtonText: 'Cancelar',
+        confirmButtonColor: '#F36F21'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            funcion()
+        }
+    })
+}
+
+function soloNumeros(input) {
+    let regex = /[^0-9]/gi;
+    input.value = input.value.replace(regex, '');
+}
+
+function mayusculas(input) {
+    input.value = input.value.toUpperCase()
+}
+function minusculas(input) {
+    input.value = input.value.toLowerCase()
+}
+
+function ajaxPost(url, data, callback) {
+    $('#loader').show()
+    $.ajax({
+        type: 'POST',
+        url: url,
+        dataType: "json",
+        data: data,
+        success: function (data) {
+            $('#loader').hide()
+            callback(data)
+        },
+        error: function (jqXHR) {
+            muestraMensaje('error', 'Ocurrio un error', 'Contacte al administrador')
+            console.log(jqXHR.responseText)
+            $('#loader').hide()
+        }
+    })
+}
+
+function ajaxPostFiles(url, data, callback) {
+    $('#loader').show()
+    $.ajax({
+        type: 'POST',
+        url: url,
+        dataType: "json",
+        data: data,
+        contentType: false,
+        processData: false,
+        success: function (data) {
+            $('#loader').hide()
+            callback(data)
+        },
+        error: function (jqXHR) {
+            muestraMensaje('error', 'Ocurrio un error', 'Contacte al administrador')
+            console.log(jqXHR.responseText)
+            $('#loader').hide()
+        }
+    })
+}
---
+++ SolAgenda/WebAgenda/Controllers/HomeController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAgenda.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        public ActionResult Noticias()
+        {
+            return View();
+        }
+
+        public ActionResult Eventos()
+        {
+            return View();
+        }
+        public ActionResult Contacto_y_Soporte()
+        {
+            return View();
+        }
+
+        public ActionResult Iniciar_Sesion()
+        {
+            return View();
+        }
+
+        public ActionResult Cerrar_Sesion()
+        {
+            return View();
+        }
+
+        public ActionResult Sesion_Iniciada()
+        {
+            return View();
+        }
+
+        public ActionResult Soberanias()
+        {
+            return View();
+        }
+
+        public ActionResult Gran_Oriente()
+        {
+            return View();
+        }
+
+        public ActionResult Logia()
+        {
+            return View();
+        }
+
+        public ActionResult Miembros_de_Logia()
+        {
+            return View();
+        }
+
+        public ActionResult Valida_tu_LodgeCard()
+        {
+            return View();
+        }
+
+    }
+}
